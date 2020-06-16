b6f6a53009d6adbdf02db0c01b9bcccb26e76ea7
Mario Fernandez Serrano <me.fernandezserrano@gmail.com>
2020-06-16 10:30:00 -0500

    Added template folder created with header-nav

---
+++ src/templates/Header.js
@@ -0,0 +1,23 @@
+const Header = () => {
+  const view = `
+    <div class="Header-main">
+      <div class="Header-logo">
+        <h1>
+          <a href="/">
+            Cientify
+          </a>
+        </h1>
+      </div>
+      
+      <div class="Header-nav">
+        <a href="#/about/">
+          ABOUT
+        </a>
+      </div>
+    </div>
+  `;
+  
+  return view;
+}
+
+export default Header;
